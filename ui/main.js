c9a6e44fbbc0b1186da297267c347c8f45cac037
harishankar90 <30753303+harishankar90@users.noreply.github.com>
2017-08-12 14:46:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,10 @@
 console.log('Loaded!');
+
+var element=document.getElementById('main-text');
+element.innerHTML = 'New value';
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+    
+    img.style.marginLeft='100px';
+}
