1ed2a957a4da52ddcd9a3e9210751ca6618c40e0
harishankar90 <30753303+harishankar90@users.noreply.github.com>
2017-08-14 13:16:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,8 @@ button.onclick = function () {
  };
    
   // Make the request
+  var nameInput = document.getElementById('name');
+var name = nameInput.value;
   request.open('GET', 'http://harishankar714.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
 
