85f4234c5eb7824df8a2b23e9a01d77327098d1d
harishankar90 <30753303+harishankar90@users.noreply.github.com>
2017-08-14 11:54:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,7 @@ button.onclick = function () {
  };
    
   // Make the request
-  request.open('GET', 'http://harishankar714.imad.hasura.io/counter', true);
+  request.open('GET', 'http://harishankar714.imad.hasura-app.io/counter', true);
   request.send(null);
 
     
