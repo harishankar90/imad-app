2d41aa850ff8d4b70648bf56eeecd2fac2e45248
harishankar90 <30753303+harishankar90@users.noreply.github.com>
2017-08-20 13:24:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,19 @@ app.get('/submit-name', function (req,res) {
     
 });
 
-   
+ var pool = new pool(config);
+ app.get('/test-db',function (req,res) {
+   pool.query.('SELECT * FROM test', function (err,result) {
+       if (err)
+       {
+           red.status(500).send(err.toString());
+       }
+       else{
+               res.send(JSON.stringify(result));
+           
+           }
+   });  
+ });
 
 
 app.get('/ui/style.css', function (req, res) {
