db3b4d7adc1dc30eb4d685ba9443a2c231c7f404
harishankar90 <30753303+harishankar90@users.noreply.github.com>
2017-08-20 13:13:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool=require('pg').Pool;
+
+var config = {
+    user: 'harishankar714',
+    databse : 'harishankar714',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password : process.env.DB.PASSWORD
+ };
+
 var app = express();
 app.use(morgan('combined'));
 
